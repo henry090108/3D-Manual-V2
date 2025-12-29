9fd34d9c1e0710a67fd2448cac5cbae5740e2681
henry090108 <125419262+henry090108@users.noreply.github.com>
2025-12-30 02:06:50 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  env: {
+    // Google Apps Script
+    GAS_URL: process.env.GAS_URL,
+    GAS_SECRET_KEY: process.env.GAS_SECRET_KEY,
+
+    // 클라이언트에서 사용하는 공개 환경변수
+    NEXT_PUBLIC_GAS_URL: process.env.GAS_URL,
+    NEXT_PUBLIC_GAS_SECRET_KEY: process.env.GAS_SECRET_KEY,
+  },
 };
 
 export default nextConfig;
